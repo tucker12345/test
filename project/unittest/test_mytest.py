1d112cc5c687f60a1a200b694b86c31b19d14653
tucker12345 <takashi.canada.toronto@gmail.com>
2025-02-01 18:20:43 -0500

    readded test_mytest.py

---
+++ project/unittest/test_mytest.py
@@ -0,0 +1,16 @@
+import pytest
+from pyspark.sql import SparkSession
+from project.Mytest import Mytest
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession.builder.appName("test").getOrCreate()
+    )
+    return spark
+
+def test_mytest(spark):
+    n = Mytest.Mytest.Mytest_method(spark)
+    assert n == 2
+    
+    
