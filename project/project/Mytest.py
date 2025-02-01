de253e38894ebd1f54bd9cbab54a29e539377760
tucker12345 <takashi.canada.toronto@gmail.com>
2025-02-01 03:10:21 -0500

    first commit

---
+++ project/project/Mytest.py
@@ -0,0 +1,13 @@
+# import pyspark
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("MyApp").getOrCreate()
+
+class Mytest():
+    
+    @classmethod
+    def Mytest_method(cls,_spark):
+        df = _spark.createDataFrame([(1, 'Alice'), (2, 'Bob')], ['id', 'name'])
+        return len(df.schema.fields)
+    
+    
+    
---
+++ project/unittest/test_Mytest.py
@@ -0,0 +1,16 @@
+import pytest
+from pyspark.sql import SparkSession
+from project.Mytest import Mytest
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = (
+        SparkSession.builder.appName("PyDeequTest").getOrCreate()
+    )
+    return spark
+
+def test_Mytest(spark):
+    n = Mytest.Mytest_method(spark)
+    assert n == 2
+    
+    
