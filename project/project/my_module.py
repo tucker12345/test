6e3484505d2c0483e85e40043e0f2f68fdad9ab6
tucker12345 <takashi.canada.toronto@gmail.com>
2025-02-07 01:03:32 -0500

    clean up code

---
+++ project/project/my_module.py
@@ -0,0 +1,34 @@
+"""test classes.
+
+This module provides:
+- copute field count;
+- calculate dataframe count.
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+
+spark = SparkSession.builder.appName("MyApp").getOrCreate()
+
+
+class MyTest:
+    """MyTest class.
+
+    Define 2 methods.
+    """
+
+    @classmethod
+    def mytest_method(cls, _df: DataFrame) -> int:
+        """_spark current spark.
+
+        returns length of fields.
+        """
+        return len(_df.schema.fields)
+
+    @classmethod
+    def mytest_method2(cls, _df: DataFrame) -> int:
+        """_spark current spark.
+
+        returns dataframe count.
+        """
+        return _df.count()
---
+++ project/unitest/test_mytest.py
@@ -0,0 +1,35 @@
+"""unit test .
+
+This module provides:
+- copute field count;
+- calculate dataframe count.
+"""
+
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+import pytest
+
+from project.my_module import MyTest
+
+
+@pytest.fixture(scope="session")
+def spark() -> SparkSession:
+    """Spark setting.
+
+    returns spark.
+    """
+    return SparkSession.builder.appName("test").getOrCreate()
+
+
+def test_mytest(spark: SparkSession) -> None:
+    """Define test case.
+
+    2 cases.
+    """
+    
+    the_df = spark.createDataFrame([(1, "Alice"), (2, "Bob")], ["id", "name"])
+    
+    n = MyTest.mytest_method(the_df)
+    assert n == 2
+    n = MyTest.mytest_method2(spark)
+    assert n == 2
